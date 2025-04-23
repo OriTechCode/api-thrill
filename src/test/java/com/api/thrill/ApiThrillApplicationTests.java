0da346d72f4c6a03870a5440d5d38711571754e4
Orihuelaa <oritechcode@gmail.com>
2025-04-22 21:25:44 -0300

    Initial commit

---
+++ src/test/java/com/api/thrill/ApiThrillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.thrill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiThrillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
