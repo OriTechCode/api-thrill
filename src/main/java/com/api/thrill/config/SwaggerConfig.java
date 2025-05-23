f8d9c99d2eb96a28d3384404b3acdd495c4d7c9b
MaxonCoreCode <taticeverino@gmail.com>
2025-05-23 09:12:20 -0300

    fusion de los cracks

--- src/main/java/com/api/thrill/config/SwaggerConfig.java
+++ src/main/java/com/api/thrill/config/SwaggerConfig.java
@@ -1,4 +1,32 @@
 package com.api.thrill.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Thrill API")
+                        .description("Documentación de la API de Thrill E-commerce")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Soporte de Thrill")
+                                .email("soporte@thrill.com")
+                                .url("https://thrill.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Documentación completa")
+                        .url("https://thrill.com/docs"));
+    }
 }
