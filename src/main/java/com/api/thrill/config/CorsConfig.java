c7a73054e439f3a7011ec32e52bf37b3f957f419
MaxonCoreCode <taticeverino@gmail.com>
2025-06-30 19:36:49 -0300

    configuracion para q el cors me tome la ruta

--- src/main/java/com/api/thrill/config/CorsConfig.java
+++ src/main/java/com/api/thrill/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://thrillweb.netlify.app/") // Desarrollo y Producción
+                        .allowedOrigins("http://localhost:5173", "https://thrillweb.netlify.app") // Desarrollo y Producción
 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos
                         .allowedHeaders("Content-Type", "Authorization", "X-Requested-With", "Accept") // Cabeceras explícitas
